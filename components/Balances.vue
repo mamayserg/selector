a66d3ca4ffa0e128c4e7ccf1e41884f15d3a03cb
Mamai <mamayserg1@gmail.com>
2019-05-10 21:19:05 +0300

    add sort method

--- components/Balances.vue
+++ components/Balances.vue
@@ -1,21 +1,28 @@
 <template>
     <div class="box-balances">
         <div class="text-balance">
-            <a href="#" @click=""> <img  class="img-arrow" src="../assets/img/white-arrow-png-41944.png"/></a>
+            <a href="#" @click="sortBy('balance_available')"> <img  class="img-arrow" src="../assets/img/white-arrow-png-41944.png"/></a>
         BALANCES
         </div>
 
         <div class="table-balances">
             <table >
-                <tr >
-                    <th><button class="sort-buttons-box">ff</button></th>
-                    <th><button class="sort-buttons-box">ff</button></th>
-                    <th><button class="sort-buttons-box">ff</button></th>
+                <tr  >
+                    <th v-for="sort in sorts">
+                        <div class="container-button">
+                        <div class="sort-buttons-box">
+                            <div ><a href="#" @click="sortBy(sort.name,sort.desc)"><img  class="sort-arrow" src="../assets/img/up.png"/></a></div>
+                            <div ><a href="#" @click="sortBy(sort.name,sort.asc)"><img  class="sort-arrow" src="../assets/img/down.png"/></a></div>
+                        </div>
+                        <p id="button-name">{{sort.buttonName}}</p>
+                        </div>
+                    </th>
+
                 </tr>
-                <tr v-for=" balance in myBalance" v-bind:key="balance.id">
-                    <th ><img :src="balance.img"/> {{balance.user_id}}</th>
-                    <th>{{balance.balance_available}}</th>
+                <tr v-for=" balance in orderedUsers " v-bind:key="balance.id">
+                    <th ><img src="../assets/img/BNB.png"  /> {{balance.name}}</th>
                     <th>{{balance.balance}}</th>
+                    <th>{{balance.balance_available}}</th>
 
                 </tr>
 
@@ -29,15 +36,30 @@
     export default {
         data(){
             return{
-
+                in:'BNB.png',
+                sorts:[{name:'name', desc:'desc',asc:'asc',buttonName:''},
+                    {name:'balance', desc:'desc',asc:'asc',buttonName:'ALL'},
+                    {name:'balance_available', desc:'desc',asc:'asc',buttonName:'AVAILABLE'}],
+                orderBy:'name',
+                orderId:'asc'
             }
         },
         computed:{
+            orderedUsers () {
+                return _.orderBy(this.$store.state.balances, this.orderBy,this.orderId)
+            },
           myBalance(){
               return this.$store.state.balances
           }
+
         },
         methods:{
+            sortBy(order,by){
+                this.orderBy=order;
+                this.orderId=by;
+
+                console.log(order,by);
+            },
           ball(){
               console.log(this.$store.state.balances);
           }
@@ -56,14 +78,34 @@
         padding:15px ;
         margin-left: 15px;
         color: white;
-     }
+    }
+
+    #button-name{
+        padding-top: 16px;
+        padding-left: 6px;
+        color: darkgrey;
+        font-size: 16px;
+        font-style: normal;
+        font-family: sans-serif ;
+
+    }
     .table-balances{
         margin: 1px;
     }
+    .container-button{
+        display: flex;
+        flex-direction:row;
+    }
     .sort-buttons-box{
-        padding: 10px;
+        display: flex;
+        flex-direction:column;
+        padding: 0px;
         margin-left: 50px;
 
+    }
+    .sort-arrow{
+        width: 25px;
+        height: 25px;
     }
     .img-arrow{
        margin-top: 5px;
--- store/index.js
+++ store/index.js
@@ -6,18 +6,16 @@ Vue.use(Vuex)
 const store = () => new Vuex.Store({
 
     state: {
+
        balances:[
-{ id: '1',img:"../assets/img/BNB.png", user_id: '18', balance: '9068629030347566', balance_available: '9068138330347570'},
-{id: '2', user_id: '18', balance: '8999948067073309', balance_available: '3000002167012309'},
-{id: '3', user_id: '18', balance: '9000000359875508', balance_available: '8999956954675509' },
-{id: "4", user_id: '18', balance: '9000000359875508', balance_available: '8999956954675509'},
-{id: "5", user_id: '18', balance: '9000000359875508', balance_available: '8999956954675509'},
-{id: "6", user_id: '18', balance: '9000000359875508', balance_available: '8999956954675509'},
-{id: "7", user_id: '18', balance: '9000000359875508', balance_available: '54675509'},
-{id: "8", user_id: '18', balance: '9000000359875508', balance_available: '8999956954675509'},
-{id: "9", user_id: '18', balance: '08', balance_available: '8999956954675509'},
-{id: 10, user_id: '18', balance: '9000000359875508', balance_available: '8999956954675509'},
-{id: 11, user_id: '18', balance: '5', balance_available: '75509'}
+{ id: '1', name:'USD', img: '../assets/img/BNB.png ', user_id: '18', balance: '9068629030347566', balance_available: 9068138330347570},
+{id: '2',name:'BTC', user_id: '18', balance: 8999948067073309, balance_available: 3000002167012309},
+{id: '3',name:'ETH', user_id: '18', balance: 5700050359875508, balance_available: 8699985554675509 },
+{id: "4",name:'LTC', user_id: '18', balance: 9005700359875508, balance_available: 8559956954675509},
+{id: "5",name:'ZEC', user_id: '18', balance: 580000035988, balance_available: 8999956954675509},
+{id: "6",name:'DASH', user_id: '18', balance: 90800000359875508, balance_available: 8999956954675509},
+{id: "7",name:'EDR', user_id: '18', balance: 9004400359875508, balance_available: 5467554409},
+{id: "8",name:'MNC', user_id: '18', balance: 950000359875508, balance_available: 6399556954675509}
        ]
     },
     mutations: {
